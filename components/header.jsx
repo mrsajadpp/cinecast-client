5756a3e72c4810afc69854958d02d620137b6ca7
mrsajadpp <mrsajadpp@gmail.com>
2024-11-02 19:35:19 +0530

    mobile header added

--- components/header.jsx
+++ components/header.jsx
@@ -1,18 +1,20 @@
 import Link from "next/link";
+import '@/public/css/header.css'
 
 export default function Header() {
     return (
         <>
+            {/* Mobile Header */}
             <header>
                 <div className="left">
                     <div className="menuIcon">
                         <button>
-                            <i class="fi fi-rr-menu-burger"></i>
+                            <i class="fi fi-ss-menu-burger"></i>
                         </button>
                     </div>
                     <div className="logo">
                         <Link href="/">
-                            <img src="/logo/black-text-logo.png" alt="CineCast" />
+                            <img src="/logo/white-text-logo.png" alt="CineCast" />
                         </Link>
                     </div>
                 </div>

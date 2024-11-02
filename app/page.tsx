139295090c057ccbd54474197a873fa1b6f7b0fe
mrsajadpp <mrsajadpp@gmail.com>
2024-11-02 22:17:25 +0530

    contact form added

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,42 @@ export default async function App() {
           &nbsp;Effortlessly generate engaging paragraphs, streamline text summarization, create detailed descriptions, and enhance your content with our AI-driven solutions. Join us today and elevate your content creation experience!
         </Marquee>
       </div>
+      <div className="contact">
+        <form>
+          <div className="row">
+            <h2>Contact Us</h2>
+          </div>
+          <div className="row">
+            <div className="inputBox">
+              <label htmlFor="firstname">First Name:*</label>
+              <input type="text" id="firstname" name="firstname" required />
+            </div>
+            &nbsp;&nbsp;
+            <div className="inputBox">
+              <label htmlFor="lastname">Last Name:*</label>
+              <input type="text" id="lastname" name="lastname" required />
+            </div>
+          </div>
+          <div className="row">
+            <div className="inputBox">
+              <label htmlFor="email">Email:*</label>
+              <input type="email" id="email" name="email" required />
+            </div>
+          </div>
+          <div className="row">
+            <div className="inputBox messageBox">
+              <label htmlFor="message">Message:*</label>
+              <textarea name="message" id="message" required></textarea>
+            </div>
+          </div>
+          <div className="row">
+            <div className="inputBox">
+              <button type="submit">Send</button>
+            </div>
+          </div>
+        </form>
+      </div>
+      <br /><br />
     </>
   );
 }

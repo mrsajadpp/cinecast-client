118f3bdc5992028c0da19bc67fec6923ca1c543c
mrsajadpp <mrsajadpp@gmail.com>
2024-11-02 21:04:09 +0530

    bug fixed

--- components/footer.jsx
+++ components/footer.jsx
@@ -15,7 +15,7 @@ export default function Footer() {
 
                 {/* Copyright */}
                 <p className="footer-copyright">
-                    &copy; {new Date().getFullYear()} Cinecast. All rights reserved.
+                    &copy; {new Date().getFullYear()} Grovix lab. All rights reserved.
                 </p>
             </div>
         </footer>

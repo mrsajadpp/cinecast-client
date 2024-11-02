00710bca00e3794cfee22a40eae0f7555b9b90cf
mrsajadpp <mrsajadpp@gmail.com>
2024-11-02 21:39:11 +0530

    meta description added

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,15 @@ import Image from 'next/image';
 import '@/public/css/index.css';
 
 export const metadata = {
-  title: "Find Your Perfect Articles, Every Time.",
-  description: "Start reading now on Grovix Lab! Explore endless knowledge in your favorite category."
+  title: "AI Content Generation Platform",
+  description: "Discover Cinecast, the leading AI content generation platform that simplifies paragraph creation, text summarization, and description generation. Enhance your writing efficiency and produce high-quality content with ease. Join us today and transform your creative process!"
 };
 
 export default async function App() {
   return (
     <>
       <div className="container">
-        <h1 className="h1">Welcome to Our Platform</h1>
+        <h1 className="h1">AI-Powered Content Creation</h1>
         <p className="p">Cinecast empowers creators with AI-driven content generation. Effortlessly craft compelling descriptions, summaries, and more—all in one place. Unlock your creativity with Cinecast.</p>
         <button className="btn">Get Started</button>
       </div>

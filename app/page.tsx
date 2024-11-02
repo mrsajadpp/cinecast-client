27c71b17ecce16aa8bfcee8e4eaf04da360c8918
mrsajadpp <mrsajadpp@gmail.com>
2024-11-02 21:36:52 +0530

    bug fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import "@fontsource/poppins";
 import Script from 'next/script';
 import Image from 'next/image';
+import '@/public/css/index.css';
 
 export const metadata = {
   title: "Find Your Perfect Articles, Every Time.",
@@ -11,7 +12,9 @@ export default async function App() {
   return (
     <>
       <div className="container">
-
+        <h1 className="h1">Welcome to Our Platform</h1>
+        <p className="p">Cinecast empowers creators with AI-driven content generation. Effortlessly craft compelling descriptions, summaries, and more—all in one place. Unlock your creativity with Cinecast.</p>
+        <button className="btn">Get Started</button>
       </div>
     </>
   );
